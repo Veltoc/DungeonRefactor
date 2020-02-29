f7fef9df2f37aeccce2bc0ae0a2324d59e6a135a
Mason <47073732+Veltoc@users.noreply.github.com>
2020-02-29 15:06:48 -0800

    Refactor #3, fixing all the public access

---
+++ Thief.java
@@ -0,0 +1,45 @@
+
+
+/**
+ * Title:
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:
+ * @author
+ * @version 1.0
+ */
+
+class Thief extends Hero
+{
+
+    public Thief()
+    {
+        super("Thief", 75, 6, .8, 20, 40, .5,"","Surprise Attack");
+
+
+
+    }//end constructor
+
+    public void special(DungeonCharacter opponent)
+    {
+        double surprise = Math.random();
+        if (surprise <= .4)
+        {
+            System.out.println("Surprise attack was successful!\n" +
+                    name + " gets an additional turn.");
+            numTurns++;
+            attack(opponent);
+        }//end surprise
+        else if (surprise >= .9)
+        {
+            System.out.println("Uh oh! " + opponent.getName() + " saw you and" +
+                    " blocked your attack!");
+        }
+        else
+            attack(opponent);
+
+
+    }//end surpriseAttack method
+
+
+}
